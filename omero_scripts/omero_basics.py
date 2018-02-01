9c7adbd92ba18642c339cdb7ffacfdfe87239f5b
Jeremy Muhlich <jmuhlich@bitflood.org>
2018-02-01 15:45:19 -0500

    Add OMERO CLI session support  (#20)
    Add OMERO CLI session support

--- omero_scripts/omero_basics.py
+++ omero_scripts/omero_basics.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import ConfigParser
+from omero.util.sessions import SessionsStore
 from omero.gateway import BlitzGateway
 from omero.sys import ParametersI
 from csv import writer, QUOTE_NONE
@@ -11,34 +12,56 @@ class OMEROConnectionManager:
         a connection will attempt to connect if connect was not already
         successfuly executed '''
 
-    def __init__(self, config_file='~/.omero/config'):
-
-        # Expand any user directory in config file
-        config_file = os.path.expanduser(config_file)
-
-        # Check config file exists
-        if not (os.path.exists(config_file) and os.path.isfile(config_file)):
-            sys.stderr.write('Configuration file {} must '
-                             'exist\n'.format(config_file))
-            sys.exit(1)
-
-        # Check permisisons on config file
-        if os.stat(config_file).st_mode & 0077:
-            sys.stderr.write('Configure file containing private credentials '
-                             'should be 600. Please run '
-                             'chmod 600 {}\n'.format(config_file))
-            sys.exit(1)
-
-        # Read the credentials file
-        config = ConfigParser.RawConfigParser()
-        config.read(config_file)
-        self.HOST = config.get('OMEROCredentials', 'host')
-        self.PORT = config.getint('OMEROCredentials', 'port')
-        self.USERNAME = config.get('OMEROCredentials', 'username')
-        self.PASSWORD = config.get('OMEROCredentials', 'password')
+    def __init__(self, config_file=None):
 
         # Set the connection as not established
         self.conn = None
+
+        self.SUUID = None
+        self.HOST = None
+        self.PORT = None
+        self.USERNAME = None
+        self.PASSWORD = None
+
+        # If config_file not specified, first see if there's an active OMERO
+        # CLI session.
+        if config_file is None:
+            store = SessionsStore()
+            session_props = store.get_current()
+            self.HOST, self.USERNAME, self.SUUID, self.PORT = session_props
+
+        # config_file specified, or no active session. Continue with reading
+        # connection params from the config file.
+        if config_file is not None or self.SUUID is None:
+
+            # Normalize file path.
+            if config_file is None:
+                config_file = '~/.omero/config'
+            config_file = os.path.expanduser(config_file)
+
+            # Check config file exists.
+            if not (os.path.exists(config_file)
+                    and os.path.isfile(config_file)):
+                sys.stderr.write('No active OMERO CLI session and '
+                                 'configuration file {} does not '
+                                 'exist\n'.format(config_file))
+                sys.exit(1)
+
+            # Check permisisons on config file.
+            if os.stat(config_file).st_mode & 0077:
+                sys.stderr.write('Configuration file contains private '
+                                 'credentials and must not be accessible by '
+                                 'other users. Please run:\n\n'
+                                 '    chmod 600 {}\n\n'.format(config_file))
+                sys.exit(1)
+
+            # Read the credentials file.
+            config = ConfigParser.RawConfigParser()
+            config.read(config_file)
+            self.HOST = config.get('OMEROCredentials', 'host')
+            self.PORT = config.getint('OMEROCredentials', 'port')
+            self.USERNAME = config.get('OMEROCredentials', 'username')
+            self.PASSWORD = config.get('OMEROCredentials', 'password')
 
     def connect(self):
         ''' Create an OMERO Connection '''
@@ -47,14 +70,17 @@ class OMEROConnectionManager:
         if self.conn is not None:
             return self.conn
 
-        # Initialize the connection
-        self.conn = BlitzGateway(self.USERNAME,
-                                 self.PASSWORD,
+        # Initialize the connection. At least HOST and PORT will be defined,
+        # but USERNAME and PASSWORD may be None if we are connecting to an
+        # existing session via its uuid.
+        self.conn = BlitzGateway(username=self.USERNAME,
+                                 passwd=self.PASSWORD,
                                  host=self.HOST,
                                  port=self.PORT)
 
-        # Connect
-        connected = self.conn.connect()
+        # Connect. If USERNAME and PASSWORD are None then SUUID must be
+        # defined.
+        connected = self.conn.connect(sUuid=self.SUUID)
 
         # Check that the connection was established
         if not connected:
@@ -65,7 +91,7 @@ class OMEROConnectionManager:
 
     def disconnect(self):
         ''' Terminate the OMERO Connection '''
-        self.conn._closeSession()
+        self.conn.seppuku(softclose=True)
         self.conn = None
 
     def hql_query(self, query, params=None):
@@ -101,6 +127,9 @@ class OMEROConnectionManager:
             unwrapped_rows.append(unwrapped_row)
 
         return unwrapped_rows
+
+    def __del__(self):
+        self.disconnect()
 
 
 def write_csv(rows, filename, header=None):
